844de11baa9ca902667aca2ef8dc054ea7dceb9c
juanmahecha9 <juanmahecha.dc@gmail.com>
2024-06-05 22:50:29 -0500

    debug

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def default():
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
